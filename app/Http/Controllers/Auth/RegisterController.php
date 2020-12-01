e346115deee52b17ff4cd68554e2c9decf755105
gabriel silva <sr.silvagabriel@gmail.com>
2020-12-01 20:05:38 -0300

    criação de user com tenant ok

--- app/Http/Controllers/Auth/RegisterController.php
+++ app/Http/Controllers/Auth/RegisterController.php
@@ -3,11 +3,13 @@
 namespace App\Http\Controllers\Auth;
 
 use App\Http\Controllers\Controller;
+use App\Model\Tenant;
 use App\Providers\RouteServiceProvider;
 use App\Model\User;
 use Illuminate\Foundation\Auth\RegistersUsers;
 use Illuminate\Support\Facades\Hash;
 use Illuminate\Support\Facades\Validator;
+use Webpatser\Uuid\Uuid;
 
 class RegisterController extends Controller
 {
@@ -53,6 +55,7 @@ class RegisterController extends Controller
             'name' => ['required', 'string', 'max:255'],
             'email' => ['required', 'string', 'email', 'max:255', 'unique:users'],
             'password' => ['required', 'string', 'min:8', 'confirmed'],
+            'tenant' => ['required', 'string', 'max:255'],
         ]);
     }
 
@@ -64,7 +67,16 @@ class RegisterController extends Controller
      */
     protected function create(array $data)
     {
-        return User::create([
+        $tenant = Tenant::where('name', $data['tenant'])->first();
+
+        if (is_null($tenant)) {
+            $tenant = Tenant::create([
+                'name' => $data['tenant'],
+                'uuid' => Uuid::generate(4)
+            ]);
+        }
+
+        return $tenant->User()->create([
             'name' => $data['name'],
             'email' => $data['email'],
             'password' => Hash::make($data['password']),
